adfb0bf722a2a43f03d5ad3f79d3cfd5a7fb20c4
nikbold <nik.bold@zynstra.com>
2021-10-04 11:01:52 +0100

    Working Script
    Working script that connects to the vpn, opens a browser with specific tabs open, opens wsl. slack. outlook and Teams.

--- startup.py
+++ startup.py
@@ -1,17 +1,15 @@
 # Nik's Startup Script
 
-#### What Script has to do:
+# Load modules:
+import os
+import webbrowser
 
 ## VPN
-# Connect to Full-Tunnel VPN
-
-import os
+# Connect to Dev VPN
 openvpn =r'"C:\Program Files\OpenVPN\bin\openvpn-gui.exe" --connect Zynstra-Bath-full.ovpn'
 os.system(openvpn)
 
 ## Chrome
-import webbrowser
-
 url = 'https://github.com/nikbold/' # Open GIT:
 url1 = 'http://192.168.11.10/glpi/' # GLPI
 url2 = 'https://github.com/nikbold?tab=repositories' # Open CI Team Board:
@@ -24,21 +22,17 @@ webbrowser.open_new(url3)
 ## WSL2
 
 # Open New Window
+os.startfile("wsl")
 
 ## Outlook
 # Scrape new emails
-# Open Outlook
-import os
-#os.startfile("outlook")
-import psutil ## https://topic.alibabacloud.com/a/installing-psutil-430-in-windows-10-with-pip_1_15_30862932.html#:~:text=%20Installing%20Psutil%204.3.0%20in%20Windows%2010%20with,use%20CMD%20to%20install%203.5%20psutil%20More%20
 
-def is_outlook_running():
-    for p in psutil.process_iter(attrs=['pid', 'name']):
-        if "OUTLOOK.EXE" in p.info['name']:
-            print("Yes", p.info['name'], "is running")
-            break
-    else:
-        print("No, Outlook is not running")
-        os.startfile("outlook")
-        print("Outlook is starting now...")
+# Open Outlook
+os.startfile("outlook")
+
 ## Open Slack
+slack =r'"C:\Users\NB250346\AppData\Local\slack\slack.exe"'
+os.startfile(slack)
+
+## Open Teams
+os.startfile(r"C:\Users\NB250346\AppData\Local\Microsoft\Teams\current\Teams.exe")
